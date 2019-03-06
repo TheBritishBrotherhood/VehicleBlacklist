4f962039e14e08f1a20766095763a8734237d994
TheBritishBrotherhood <TheBritishBrotherhood@users.noreply.github.com>
2019-03-06 21:45:40 +0000

    Update s_car.lua

--- VehicleBlacklist/server/s_car.lua
+++ VehicleBlacklist/server/s_car.lua
@@ -4,9 +4,10 @@ RegisterServerEvent("s_RemoveAllWeapons")
 
 -- Only allow users with permission level 1 or higher to drive blacklisted vehicle.
 AddEventHandler("s_IsCarDriveable", function()
+  local source = source
   TriggerEvent('es:getPlayerFromId', source, function(user)
     if user then
-    if ( user.permission_level >= 1 ) then
+    if ( user.getPermissions >= 1 ) then
        TriggerClientEvent("c_Driveable", source)
       else
        TriggerClientEvent("c_Undriveable", source)
@@ -17,9 +18,10 @@ end)
 
 -- remove civilians weapons when exiting any blacklisted vehicle.
 AddEventHandler("s_RemoveAllWeapons", function()
+  local source = source
   TriggerEvent('es:getPlayerFromId', source, function(user)
     if user then
-      if ( user.permission_level == 0 ) and (db.driver == "mysql") then
+      if ( user.getPermissions == 0 ) and (db.driver == "mysql") then
         MySQL:executeQuery("DELETE FROM user_weapons WHERE identifier='@identifier'", { ['@identifier'] = identifier})
           elseif ( user.permission_level == 0 ) and (db.driver == "none") then
             TriggerClientEvent("c_RemWeap", source)
