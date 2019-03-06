99984ccce06d333002f231d4c85829e5d4efeae7
TheBritishBrotherhood <TheBritishBrotherhood@users.noreply.github.com>
2019-03-06 21:48:43 +0000

    Update config_db.lua

--- VehicleBlacklist/config/config_db.lua
+++ VehicleBlacklist/config/config_db.lua
@@ -2,11 +2,5 @@
 
 db = {
     --driver = "none", << Like this.
-	driver = "mysql-async",
-
-	
-	sql_host = "127.0.0.1",
-	sql_database = "your-database-here",
-	sql_user = "root",
-	sql_password = "password"
+	driver = "mysql-async"
 }
